d63c09a960a32ebcdf537767c824551d3e95d992
dvanderblom <mijnemailadre@gmail.com>
2022-09-08 15:19:39 +0200

    Voorbereiding stageopdracht

---
+++ BlazorApp1/wwwroot/JavaScript/script.js
@@ -0,0 +1,15 @@
+﻿mapBoxFunctions = {
+    initMapBox: function() {
+        
+        mapboxgl.accessToken = 'pk.eyJ1IjoibGFpeGVyIiwiYSI6ImNsN3J5dWxmODA4djYzb2xmZXY0Y3BuOHIifQ.2QnAMlaecjI_UBRsoSc7JQ';
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [4.364940, 52.011840], // starting position [lng, lat]
+            zoom: 12.5, // starting zoom
+        });
+
+    }
+
+};
+
